11c21e55b1e66d5547516322018f32f26b3a6943
Igor Oliveira <44407469+oliveiraigordev@users.noreply.github.com>
2024-04-02 20:46:47 -0300

    implementação de uuid na tabela de usuarios

--- models/user.py
+++ models/user.py
@@ -1,8 +1,10 @@
+from uuid import uuid4
 from flask_login import UserMixin
 from database import db
 
 
 class User(db.Model, UserMixin):
-    id = db.Column(db.String(36), primary_key=True)
+    id = db.Column(db.String(36), primary_key=True,
+                   default=lambda: str(uuid4()))
     username = db.Column(db.String(80), nullable=False, unique=True)
     password = db.Column(db.String(64), nullable=False)
